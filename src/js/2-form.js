de261f566ebc84160451644a14f5b0969cfeb020
EgorOgichuk <123835482+EgorOgirchuk@users.noreply.github.com>
2024-07-23 20:05:47 +0300

    init

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+const feedbackFormStateKey = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const dataFromLocalStorage = localStorage.getItem(feedbackFormStateKey);
+let emailInput = document.querySelector('.feedback-form input[type="email"]');
+let messageTextarea = document.querySelector('.feedback-form textarea');
+
+if (dataFromLocalStorage != null) {
+  formData = JSON.parse(dataFromLocalStorage);
+  emailInput.value = formData.email;
+  messageTextarea.value = formData.message;
+}
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', () => {
+  formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(feedbackFormStateKey, JSON.stringify(formData));
+
+  console.log(formData);
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+
+  if (
+    elements.email.value.trim() === '' ||
+    elements.message.value.trim() === ''
+  ) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(feedbackFormStateKey);
+    form.reset();
+  }
+});
